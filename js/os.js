ca436056258bd94676ce20e9e4664375c76c0c0a
Ephraim Becker <emb16@outlook.com>
2015-10-01 18:16:15 -0400

    Start on the Virtual OS

--- js/os.js
+++ js/os.js
@@ -1,8 +1,20 @@
 // This is a demo of my OS running inside the CSS based Hub
 var StartOS = document.getElementById("Start-OS");
+var Hub = document.getElementById("Hub");
 
 StartOS.onclick = function VirtualHubOS()
 {
 StartOS.style.visibility = "hidden";
 
+// Setup Screen
+var Text = document.createElement("p");
+var Input = document.createElement("input");
+
+
+
+Hub.appendChild(Input);
+Input.setAttribute("type", "file");
+Input.style.position = "relative";
+Input.style.left = "675px";
+Input.style.top = "350px";
 };
